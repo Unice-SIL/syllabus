c1fe87eae454353b5b10fc3bc0aba5af6dd4ea83
ppomedio <philippe.pomedio@unice.fr>
2019-03-12 13:40:44 +0100

    “HomeAction” update.

--- src/AppBundle/Action/HomeAction.php
+++ src/AppBundle/Action/HomeAction.php
@@ -43,9 +43,8 @@ class HomeAction implements ActionInterface
     public function __invoke(Request $request)
     {
 
-        $this->session->getFlashBag()->add('success', "Check your browser console…");
+        $this->session->getFlashBag()->add('info', "Type Q/L/P/Z/I/S/W/D + <Enter>…");
 
-        #return new Response($this->templating->render('default/index.html.twig'));
-        return new Response($this->templating->render('course/edit_course.html.twig'));
+        return new Response($this->templating->render('default/index.html.twig'));
     }
 }
