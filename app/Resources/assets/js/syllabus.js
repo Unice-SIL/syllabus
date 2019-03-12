dd36a69f8ee4e52bdc8c8c3a07505e07b06a37de
ppomedio <philippe.pomedio@unice.fr>
2019-03-12 13:38:42 +0100

    Floating alerts JS.

--- app/Resources/assets/js/syllabus.js
+++ app/Resources/assets/js/syllabus.js
@@ -5,6 +5,10 @@
 */
 
 
+import $ from 'jquery';
+
+
+
 var Syllabus = ( function ( ) {
 
 
@@ -17,7 +21,42 @@ var Syllabus = ( function ( ) {
     */
 
 
-    function _ajaxTabContentLoader( $tabLink ) {
+    const MS_BEFORE_DISMISS = 4000,
+        USER_DISMISSIBLE_ALERTS = [
+                //'primary',
+                //'success',
+                //'warning',
+                'danger'
+            ];
+
+    var _$alertContainer = $( '#js-alerts_container' ),
+        _messages = { };
+
+
+    var _removeItem = function( item ) {
+
+        $( item.elem ).remove( );
+
+    };
+
+
+    var _resetMessages = function( ) {
+
+        _messages = {
+            'light': [ ],
+            'dark': [ ],
+            'secondary': [ ],
+            'primary': [ ],
+            'info': [ ],
+            'success': [ ],
+            'warning': [ ],
+            'danger': [ ]
+        };
+
+    };
+
+
+    var _ajaxTabContentLoader = function( $tabLink ) {
 
         var route = $tabLink.data( 'route' );
 
@@ -39,7 +78,59 @@ var Syllabus = ( function ( ) {
             } );
         }
 
-    }
+    };
+
+
+    var _displayAllAlerts = function( ) {
+
+        for ( var key in _messages ) {
+
+            var message = _messages[ key ];
+
+            for ( var index in message ) {
+
+                var $alert = null;
+
+                if ( USER_DISMISSIBLE_ALERTS.includes( key ) ) {
+
+                    var $button = $( "<button>", {
+                            'type': "button",
+                            'class': "close",
+                            'data-dismiss': "alert",
+                            'aria-label': "Close",
+                            'html': '<span aria-hidden="true">&times;</span>'
+                        } );
+
+                    $alert = $( "<div>", {
+                        'class': "alert alert-dismissible fade show alert-" + key,
+                        'html': message[ index ],
+                        'css': { 'display': 'none' }
+                    } );
+
+                    _$alertContainer.prepend( $alert.append( $button ) );
+                    $alert.slideDown( );
+
+                } else {
+
+                    $alert = $( "<div>", {
+                            'class': "alert alert-" + key,
+                            'html': message[ index ],
+                            'css': { 'display': 'none' }
+                        } );
+
+                    _$alertContainer.prepend( $alert );
+                    $alert.slideDown( ).delay( MS_BEFORE_DISMISS )
+                            .slideUp( {
+                                always: _removeItem,
+                            } );
+
+                }
+            }
+        }
+
+        _resetMessages( );
+
+    };
 
 
 
@@ -49,7 +140,35 @@ var Syllabus = ( function ( ) {
     */
 
 
-    function tabLoaderInit( ) {
+    var alert = function( type, text, keep ) {
+
+        if ( type === true ) {
+
+            _displayAllAlerts( );
+
+        } else {
+
+            //if ( type === undefined || ! ( type in _messages ) ) {
+            if ( type === undefined || ! _messages.hasOwnProperty( type ) ) {
+                type = 'danger';
+            }
+
+            if ( text === undefined ) {
+                text = "Une erreur est survenue.";
+            }
+
+            _messages[ type ].push( text );
+
+            if ( ! keep ) {
+                _displayAllAlerts( );
+            }
+
+        }
+
+    };
+
+
+    var tabLoaderInit = function( ) {
 
         $( 'main > .row:first-child > div > ul.nav' )
                 .on( 'click', 'li.nav-item > a', function( ) {
@@ -61,8 +180,16 @@ var Syllabus = ( function ( ) {
         $( '#tab-1' ).addClass( 'active' );
         _ajaxTabContentLoader( $( '#tab-1' ) );
 
-    }
+    };
 
+
+
+    /**************************************************************************
+
+            Init.
+    */
+
+    _resetMessages( );
 
 
     /*
@@ -70,13 +197,12 @@ var Syllabus = ( function ( ) {
     */
 
     return {
+        alert: alert,
         tabLoaderInit: tabLoaderInit
     };
 
 
-
 } ) ( );
-
 
 
 export default Syllabus;
