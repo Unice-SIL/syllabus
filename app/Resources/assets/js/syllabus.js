70502f274a33f4fd3134ee0bd25d40f96138cb6e
Joran Galano <jgalano@avencod.fr>
2019-04-03 11:07:39 +0200

    save change tab js

--- app/Resources/assets/js/syllabus.js
+++ app/Resources/assets/js/syllabus.js
@@ -68,12 +68,27 @@ var Syllabus = ( function ( ) {
 
             _ajaxTabContentLoader( $( this ) );
 
+            saveCurrentTabContent( $( this ) );
+
         } );
 
         $( '#tab-1' ).addClass( 'active' );
+        $( '#tab-1' ).addClass( 'syllabus-tab-active' );
         _ajaxTabContentLoader( $( '#tab-1' ) );
 
     };
+
+    var saveCurrentTabContent = function( tab ) {
+        if(!tab["0"].classList.contains('syllabus-tab-active')){
+            var active_tab_button = document.getElementsByClassName('syllabus-tab-active')[0];
+            var active_tab = document.getElementById("panel_"+active_tab_button.id);
+            var sumbit_button = active_tab.getElementsByClassName("submit")[0];
+            sumbit_button.click();
+            active_tab_button.classList.remove('syllabus-tab-active');
+            tab.addClass( 'syllabus-tab-active' );
+        }
+
+    }
 
 
     /*
