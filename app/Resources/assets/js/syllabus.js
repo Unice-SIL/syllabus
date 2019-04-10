fe1324a194aff0702f918b026cc4acd36c4b6d05
ppomedio <philippe.pomedio@unice.fr>
2019-04-10 12:43:39 +0200

    Not a public item.

--- app/Resources/assets/js/syllabus.js
+++ app/Resources/assets/js/syllabus.js
@@ -57,6 +57,18 @@ var Syllabus = ( function ( ) {
     };
 
 
+    var _saveCurrentTabContent = function( tab ) {
+        if(!tab["0"].classList.contains('syllabus-tab-active')){
+            var active_tab_button = document.getElementsByClassName('syllabus-tab-active')[0];
+            var active_tab = document.getElementById("panel_"+active_tab_button.id);
+            var sumbit_button = active_tab.getElementsByClassName("submit")[0];
+            sumbit_button.click();
+            active_tab_button.classList.remove('syllabus-tab-active');
+            tab.addClass( 'syllabus-tab-active' );
+        }
+    }
+
+
 
     /**************************************************************************
 
@@ -70,8 +82,7 @@ var Syllabus = ( function ( ) {
                 .on( 'click', 'li.nav-item > a', function( ) {
 
             _ajaxTabContentLoader( $( this ) );
-
-            saveCurrentTabContent( $( this ) );
+            _saveCurrentTabContent( $( this ) );
 
         } );
 
@@ -81,17 +92,6 @@ var Syllabus = ( function ( ) {
 
     };
 
-    var saveCurrentTabContent = function( tab ) {
-        if(!tab["0"].classList.contains('syllabus-tab-active')){
-            var active_tab_button = document.getElementsByClassName('syllabus-tab-active')[0];
-            var active_tab = document.getElementById("panel_"+active_tab_button.id);
-            var sumbit_button = active_tab.getElementsByClassName("submit")[0];
-            sumbit_button.click();
-            active_tab_button.classList.remove('syllabus-tab-active');
-            tab.addClass( 'syllabus-tab-active' );
-        }
-
-    }
 
     var handleAjaxResponse = function( response ) {
         if(response.messages !== undefined) {
