f0b04ffd2bd352adfbbbfc3b7fb30c1f508f2408
ppomedio <philippe.pomedio@unice.fr>
2019-05-07 15:12:19 +0200

    Add new action.

---
+++ src/AppBundle/Action/Ui/Course/AskForAdviceCourseInfoAction.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace AppBundle\Action\Ui\Course;
+
+use AppBundle\Action\ActionInterface;
+use AppBundle\Exception\CourseInfoNotFoundException;
+use AppBundle\Helper\CourseInfoHelper;
+use AppBundle\Query\Course\FindCourseInfoByIdQuery;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+/**
+ * Class PublishCourseInfoAction
+ * @package AppBundle\Action\Ui\Course
+ */
+class AskForAdviceCourseInfoAction implements ActionInterface
+{
+    /**
+     * @var FindCourseInfoByIdQuery
+     */
+    private $findCourseInfoByIdQuery;
+
+    /**
+     * @var Environment
+     */
+    private $templating;
+
+    /**
+     * @var CourseInfoHelper
+     */
+    private $courseInfoHelper;
+
+    /**
+     * @var \Swift_Mailer
+     */
+    private $mailer;
+
+    /**
+     * @var string
+     */
+    private $mailerSource;
+
+    /**
+     * @var string
+     */
+    private $mailerTarget;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * PublishCourseInfoAction constructor.
+     * @param FindCourseInfoByIdQuery $findCourseInfoByIdQuery
+     * @param Environment $templating
+     * @param CourseInfoHelper $courseInfoHelper
+     * @param \Swift_Mailer $mailer
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+            string $mailerSource,
+            string $mailerTarget,
+            FindCourseInfoByIdQuery $findCourseInfoByIdQuery,
+            Environment $templating,
+            CourseInfoHelper $courseInfoHelper,
+            \Swift_Mailer $mailer,
+            LoggerInterface $logger
+        )
+    {
+        $this->mailerSource = $mailerSource;
+        $this->mailerTarget = $mailerTarget;
+        $this->findCourseInfoByIdQuery = $findCourseInfoByIdQuery;
+        $this->templating = $templating;
+        $this->courseInfoHelper =$courseInfoHelper;
+        $this->mailer = $mailer;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * @Route("/course/ask4advice/{id}", name="ask_for_advice_course_info")
+     */
+    public function __invoke(Request $request)
+    {
+        $messages = [];
+        $renders = [];
+
+        try {
+
+            $id = $request->get('id', null);
+
+            try {
+
+                $courseInfo = $this->findCourseInfoByIdQuery->setId($id)->execute();
+
+                $message = (new \Swift_Message("[SYLLABUS] Demande d'avis."))
+                    ->setFrom($this->mailerSource)
+                    ->setTo($this->mailerTarget)
+                    ->setBody(
+                        $this->templating->render(
+                            #'email/advice.html.twig',
+                            'email/publication.html.twig',
+                            [
+                                'courseInfoId' => $courseInfo->getId(),
+                                'courseTitle' => $courseInfo->getTitle(),
+                                'user' => $courseInfo->getPublisher(),
+                            ]
+                        ),
+                        'text/html'
+                    )
+                    ->addPart(
+                        $this->templating->render(
+                            'email/publication.txt.twig',
+                            [
+                                'courseInfoId' => $courseInfo->getId(),
+                                'courseTitle' => $courseInfo->getTitle(),
+                                'user' => $courseInfo->getPublisher(),
+                            ]
+                        ),
+                        'text/plain'
+                    );
+
+                $this->mailer->send($message);
+
+                $messages[] = [
+                    'type' => "success",
+                    'message' => "L'avis a été demandé : vous allez être contacté bientôt."
+                ];
+
+            } catch (\Exception $e) {
+
+                $this->logger->error((string) $e);
+
+                $messages[] = [
+                    'type' => "danger",
+                    'message' => "Une erreur est survenue."
+                ];
+            }
+
+        } catch (CourseInfoNotFoundException $e) {
+
+            $messages[] = [
+                'type' => "danger",
+                'message' =>"Le cours n'a pas été retrouvé."
+            ];
+        }
+
+        return new JsonResponse(['messages' => $messages]);
+    }
+
+}
