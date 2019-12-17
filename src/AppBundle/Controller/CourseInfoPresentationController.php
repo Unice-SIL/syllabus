fe02d61fdfe8a9c5f29533642968c9400db3b6cb
Kevin Genes <kevin.genes@epitech.eu>
2019-12-17 16:55:12 +0100

    presentation general

--- src/AppBundle/Controller/CourseInfoPresentationController.php
+++ src/AppBundle/Controller/CourseInfoPresentationController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\CourseInfo;
 use AppBundle\Form\CourseInfo\Presentation\GeneralType;
+use AppBundle\Manager\CourseInfoManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,23 +30,49 @@ class CourseInfoPresentationController extends AbstractController
     }
 
     /**
-     * @Route("/course/{id}/presentation/general", name="course_presentation_general")
+     * @Route("/course/{id}/presentation/general/{action}", name="course_presentation_general", defaults={"action"=null}))
      *
      * @param $id
+     * @param $action
      * @param Request $request
+     * @param CourseInfoManager $manager
      * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
      */
-    public function generalAction($id, Request $request)
+    public function generalAction($id, $action, Request $request, CourseInfoManager $manager)
     {
         $em = $this->getDoctrine()->getManager();
+        /** @var CourseInfo $courseInfo */
         $courseInfo = $em->getRepository(CourseInfo::class)->find($id);
 
         $form = $this->createForm(GeneralType::class, $courseInfo);
         $form->handleRequest($request);
 
+        dump(1, $action);
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump(2);
+            if ($action === "submit")
+            {
+                throw new \Exception();
+                $manager->update($courseInfo);
+            }
+            return $this->render('course_info/presentation/view/general.html.twig', [
+                'courseInfo' => $courseInfo,
+            ]);
+        }
+
+        if ($action === "cancel")
+        {
+            return $this->render('course_info/presentation/view/general.html.twig', [
+                'courseInfo' => $courseInfo,
+            ]);
+
+        }
+
         return $this->render('course_info/presentation/form/general.html.twig', [
             'courseInfo' => $courseInfo,
             'form' => $form->createView()
         ]);
+
     }
 }
