16e8847fea217ebbbc46e7c00d01423928d35f0b
Lartist <salim.developpeur@gmail.com>
2020-01-09 17:22:58 +0100

    refacto de la sidebar admin done

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -5,6 +5,7 @@ namespace AppBundle\Twig;
 
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
@@ -15,9 +16,15 @@ class AppExtension extends AbstractExtension
         ];
     }
 
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('printActiveAdminSidebarLink', [AppRuntime::class, 'printActiveAdminSidebarLink']),
+        ];
+    }
+
     public function humanizeBoolean($boolean)
     {
-
         return $boolean ? 'Oui' : 'Non' ;
     }
 }
