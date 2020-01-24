1e16159865c0ab719cfb3694d0cd03420d115b53
Lartist <salim.developpeur@gmail.com>
2020-01-24 17:32:49 +0100

    api pour liste des années avec pagination done

--- src/AppBundle/Constant/UserRole.php
+++ src/AppBundle/Constant/UserRole.php
@@ -8,9 +8,11 @@ class UserRole
 {
     const ROLE_USER = 'ROLE_USER';
     const ROLE_ADMIN = 'ROLE_ADMIN';
+    const ROLE_API = 'ROLE_API';
 
     const ROLES = [
         self::ROLE_USER,
         self::ROLE_ADMIN,
+        self::ROLE_API,
     ];
 }
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ Encore
     .setOutputPath('web/build/')
     .setPublicPath('/build')
     .addEntry('app', './app/Resources/assets/js/app.js')
+    .addEntry('nelmio', './app/Resources/assets/js/nelmio.js')
     .addEntry('course_info_layout', './app/Resources/assets/js/course_info_layout.js')
     //.splitEntryChunks()
     .autoProvidejQuery()
