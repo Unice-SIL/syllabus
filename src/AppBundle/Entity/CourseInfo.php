7f5cb36e528a04818a1fdd46288b631d137329ad
Frédéric Casazza <frederic.casazza@unice.fr>
2020-02-10 15:05:23 +0100

    CourseAchievement serialization groups

--- src/AppBundle/Entity/CourseInfo.php
+++ src/AppBundle/Entity/CourseInfo.php
@@ -5,13 +5,13 @@ namespace AppBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as JMS;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Gedmo\Mapping\Annotation as Gedmo;
-use JMS\Serializer\Annotation as JMS;
 
 /**
  * CourseInfo
