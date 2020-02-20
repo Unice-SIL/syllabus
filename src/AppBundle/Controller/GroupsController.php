13c33bdc951e6ac8d9eb33d2b88dd57e1669b1b2
Lartist <salim.developpeur@gmail.com>
2020-02-20 15:27:54 +0100

    boutons de suppresion d'un group dans la liste des groupes done

--- src/AppBundle/Controller/GroupsController.php
+++ src/AppBundle/Controller/GroupsController.php
@@ -11,7 +11,9 @@ use AppBundle\Repository\Doctrine\GroupsDoctrineRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Lexik\Bundle\FormFilterBundle\Filter\FilterBuilderUpdaterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -52,9 +54,16 @@ class GroupsController extends Controller
             5
         );
 
+        $deleteFormsView = [];
+
+        foreach ($pagination as $group) {
+            $deleteFormsView[$group->getId()] = $this->createDeleteForm($group)->createView();
+        }
+
         return $this->render('groups/index.html.twig', array(
             'pagination' => $pagination,
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'deleteFormsView' => $deleteFormsView
         ));
     }
 
@@ -134,5 +143,49 @@ class GroupsController extends Controller
         $groups = array_unique($groups);
 
         return $this->json(['query' =>  $query, 'suggestions' => $groups, 'data' => $groups]);
+    }
+
+    /**
+     * Deletes a groups entity.
+     *
+     * @Route("/{id}", name="delete", methods={"DELETE"})
+     * @param Request $request
+     * @param Groups $group
+     * @return RedirectResponse
+     */
+    public function deleteAction(Request $request, Groups $group)
+    {
+        $form = $this->createDeleteForm($group);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($group);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('app_admin_groups_index');
+    }
+
+    /**
+     * Creates a form to delete a groups entity.
+     *
+     * @param Groups $group The group entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Groups $group)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('app_admin_groups_delete', array('id' => $group->getId())))
+            ->setMethod('DELETE')
+            ->add('submit', SubmitType::class, [
+                'label' => 'Supprimer',
+                'attr' => [
+                    'class' => 'btn btn-danger float-right'
+                ]
+            ])
+            ->getForm()
+            ;
     }
 }
