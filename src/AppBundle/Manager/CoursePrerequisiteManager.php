4ee677ff7bc28d5a9b0662feb826b36fc6b8681a
Kevin Genes <kevin.genes@epitech.eu>
2020-02-28 11:57:18 +0100

    refactor coursePrerequisite

---
+++ src/AppBundle/Manager/CoursePrerequisiteManager.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace AppBundle\Manager;
+
+use AppBundle\Entity\CoursePrerequisite;
+use AppBundle\Repository\Doctrine\CoursePrerequisiteDoctrineRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * Class CoursePrerequisiteManager
+ * @package AppBundle\Manager
+ */
+class CoursePrerequisiteManager
+{
+    /**
+     * @var \Doctrine\Persistence\ObjectRepository
+     */
+    private $em;
+
+    /**
+     * @var CoursePrerequisiteDoctrineRepository
+     */
+    private $repository;
+
+    /**
+     * ActivityManager constructor.
+     * @param EntityManagerInterface $em
+     * @param CoursePrerequisiteDoctrineRepository $repository
+     */
+    public function __construct(EntityManagerInterface $em, CoursePrerequisiteDoctrineRepository $repository)
+    {
+        $this->em = $em;
+        $this->repository = $repository;
+    }
+
+    /**
+     * @return CoursePrerequisite
+     */
+    public function new()
+    {
+        return new CoursePrerequisite();
+    }
+
+    /**
+     * @param string $id
+     * @return CoursePrerequisite|null
+     */
+    public function find(string $id): ?CoursePrerequisite
+    {
+        return $this->repository->find($id);
+    }
+
+    /**
+     * @return array
+     */
+    public function findAll(): array
+    {
+        return $this->repository->findAll();
+    }
+
+    /**
+     * @param CoursePrerequisite $coursePrerequisite
+     */
+    public function create(CoursePrerequisite $coursePrerequisite): void
+    {
+        $this->em->persist($coursePrerequisite);
+        $this->em->flush();
+    }
+
+    /**
+     * @param CoursePrerequisite $coursePrerequisite
+     */
+    public function update(CoursePrerequisite $coursePrerequisite): void
+    {
+        $this->em->flush();
+    }
+
+    /**
+     * @param CoursePrerequisite $coursePrerequisite
+     */
+    public function delete(CoursePrerequisite $coursePrerequisite): void
+    {
+        $this->em->remove($coursePrerequisite);
+        $this->em->flush();
+    }
+}
--- src/AppBundle/Repository/Doctrine/CoursePrerequisiteDoctrineRepository.php
+++ src/AppBundle/Repository/Doctrine/CoursePrerequisiteDoctrineRepository.php
@@ -3,70 +3,21 @@
 namespace AppBundle\Repository\Doctrine;
 
 use AppBundle\Entity\CoursePrerequisite;
-use AppBundle\Repository\CoursePrerequisiteRepositoryInterface;
-use Doctrine\ORM\EntityManager;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * Class CourseAchievementDoctrineRepository
  * @package AppBundle\Repository\Doctrine
  */
-class CoursePrerequisiteDoctrineRepository extends AbstractDoctrineRepository implements CoursePrerequisiteRepositoryInterface
+class CoursePrerequisiteDoctrineRepository extends ServiceEntityRepository
 {
-
     /**
      * CoursePrerequisiteDoctrineRepository constructor.
-     * @param EntityManager $entityManager
+     * @param ManagerRegistry $registry
      */
-    public function __construct(
-        EntityManager $entityManager
-    )
+    public function __construct(ManagerRegistry $registry)
     {
-        $this->entityManager = $entityManager;
+        parent::__construct($registry, CoursePrerequisite::class);
     }
-
-    /**
-     * @param string $id
-     * @return CoursePrerequisite|null
-     * @throws \Exception
-     */
-    public function find(string $id): ?CoursePrerequisite
-    {
-        $coursePrerequisite = null;
-        try{
-            $coursePrerequisite = $this->entityManager->getRepository(CoursePrerequisite::class)->find($id);
-            return $coursePrerequisite;
-        }catch (\Exception $e){
-            throw $e;
-        }
-    }
-
-
-    /**
-     * @param CoursePrerequisite $coursePrerequisite
-     * @throws \Exception
-     */
-    public function create(CoursePrerequisite $coursePrerequisite): void
-    {
-        try{
-            $this->entityManager->persist($coursePrerequisite);
-            $this->entityManager->flush();
-        }catch (\Exception $e){
-            throw $e;
-        }
-    }
-
-    /**
-     * @param CoursePrerequisite $coursePrerequisite
-     * @throws \Exception
-     */
-    public function delete(CoursePrerequisite $coursePrerequisite): void
-    {
-        try {
-            $this->entityManager->remove($coursePrerequisite);
-            $this->entityManager->flush();
-        }catch (\Exception $e){
-            throw $e;
-        }
-    }
-
 }
