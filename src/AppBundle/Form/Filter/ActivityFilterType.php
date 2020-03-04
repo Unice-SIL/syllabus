b364bac5113d93c186e328e080092cd6bfa6e011
Fred <frederic.casazza@univ-cotedazur.fr>
2020-03-04 11:58:48 +0100

    Refactor Activity controller

--- src/AppBundle/Form/Filter/ActivityFilterType.php
+++ src/AppBundle/Form/Filter/ActivityFilterType.php
@@ -33,7 +33,7 @@ class ActivityFilterType extends AbstractType
             'label' => 'app.form.activity.label.label',
             'attr' => [
                 'class' => 'autocomplete-input',
-                'data-autocomplete-path' => $this->generator->generate('app_admin.activity_autocomplete')
+                'data-autocomplete-path' => $this->generator->generate('app.admin.activity_autocomplete')
             ]
         ])
         ->add('activityTypes', ActivityTypeFilterType::class, [
