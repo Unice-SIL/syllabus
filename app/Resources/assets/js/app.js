be50e814cb4645c01c747111a5dcf02184ca5628
Kevin Genes <kevin.genes@epitech.eu>
2020-03-10 16:50:42 +0100

    scroll spy need "if" in <body>

--- app/Resources/assets/js/app.js
+++ app/Resources/assets/js/app.js
@@ -23,7 +23,7 @@ import 'bootstrap/js/dist/dropdown';
 import 'bootstrap/js/dist/index';
 import 'bootstrap/js/dist/modal';
 import 'bootstrap/js/dist/popover';
-//import 'bootstrap/js/dist/scrollspy';
+import 'bootstrap/js/dist/scrollspy';
 import 'bootstrap/js/dist/tab';
 import 'bootstrap/js/dist/toast';
 import 'bootstrap/js/dist/tooltip';
