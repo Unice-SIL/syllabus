4bbe2a9eb568f003bf4cb0d8473e13b3485ea7b2
Fred <frederic.casazza@univ-cotedazur.fr>
2020-06-24 10:55:06 +0200

    Remove JobFixture content

--- src/AppBundle/Fixture/JobFixture.php
+++ src/AppBundle/Fixture/JobFixture.php
@@ -2,9 +2,8 @@
 
 namespace AppBundle\Fixture;
 
-use AppBundle\Entity\Job;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 
 /**
  * Class JobFixture
@@ -19,29 +18,6 @@ class JobFixture extends Fixture
     public function load(ObjectManager $manager)
     {
 
-        $jobs = [
-            [
-              'label' => 'Test 2',
-              'command' => \AppBundle\Constant\Job::COMMAND_TEST_2,
-              'frequencyJobFormat' => '* * * * *',
-            ],
-            [
-                'label' => 'Test',
-                'command' => \AppBundle\Constant\Job::COMMAND_TEST,
-                'frequencyJobFormat' => '* * * * *',
-            ],
-
-        ];
-
-        foreach ($jobs as $c) {
-
-            $jobs = new Job();
-            $jobs->setLabel($c['label'])
-                ->setCommand($c['command'])
-                ->setFrequencyJobFormat($c['frequencyJobFormat']);
-            $manager->persist($jobs);
-        }
-        $manager->flush();
     }
 
 }
