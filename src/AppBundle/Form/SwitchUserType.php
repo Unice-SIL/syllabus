5ce8f95f3f65f688d910d11b47adc3db93f301cb
Kevin Genes <kevin.genes@epitech.eu>
2020-11-24 10:30:27 +0100

    select2 user

--- src/AppBundle/Form/SwitchUserType.php
+++ src/AppBundle/Form/SwitchUserType.php
@@ -3,10 +3,12 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Tetranz\Select2EntityBundle\Form\Type\Select2EntityType;
 
 /**
  * Class SwitchUserType
@@ -14,11 +16,37 @@ use Symfony\Component\Form\FormBuilderInterface;
  */
 class SwitchUserType extends AbstractType
 {
+    /**
+     * @var RequestStack
+     */
+    private $request;
+
+    /**
+     * SwitchUserType constructor.
+     * @param RequestStack $requestStack
+     */
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->request = $requestStack->getCurrentRequest();
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('username', TextType::class, [
+        $builder->add('username', Select2EntityType::class, [
+            'label' => 'app.form.switch_user.username',
+            'multiple' => false,
+            'remote_route' => 'app.common.autocomplete.generic_s2_user',
+            'class' => User::class,
+            'text_property' => 'getSelect2Name',
+            'page_limit' => 10,
+            'placeholder' => 'app.permission.modal.placeholder',
+            'language' => $this->request->getLocale(),
+            'minimum_input_length' => 4,
             'required' => true,
-            'label' => "app.form.switch_user.username"
         ])
             ->add('submit', SubmitType::class, [
                 'label' => 'app.form.switch_user.imitate'
