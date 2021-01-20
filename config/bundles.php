34238dd9f293ccf237638ac320034b9b03715bde
Kevin Genes <kevin.genes@epitech.eu>
2021-01-20 16:35:48 +0100

    errors config fixes

--- config/bundles.php
+++ config/bundles.php
@@ -25,6 +25,4 @@ return [
     Misd\LinkifyBundle\MisdLinkifyBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
