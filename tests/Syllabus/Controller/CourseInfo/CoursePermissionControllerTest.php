05fe7e8dfc27b6e5e4c42d3a530629135856f864
IBEGHOUCHENE <Nadir.IBEGHOUCHENE@univ-cotedazur.fr>
2022-02-25 10:07:05 +0100

    test: CoursePermissionController

--- tests/Syllabus/Controller/CourseInfo/CoursePermissionControllerTest.php
+++ tests/Syllabus/Controller/CourseInfo/CoursePermissionControllerTest.php
@@ -4,7 +4,10 @@
 namespace Tests\Syllabus\Controller\CourseInfo;
 
 use App\Syllabus\Constant\Permission;
+use App\Syllabus\Entity\CoursePermission;
 use App\Syllabus\Exception\CourseNotFoundException;
+use App\Syllabus\Fixture\CourseFixture;
+use App\Syllabus\Fixture\CoursePermissionFixture;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -49,5 +52,86 @@ class CoursePermissionControllerTest extends AbstractCourseInfoControllerTest
     {
         $this->tryWithoutPermission(self::ROUTE_APP_COURSE_PERMISSION_INDEX);
         $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+    }
+
+    public function testAddPermissionSectionSuccessful()
+    {
+        $this->login();
+        $course = $this->getCourse(CourseFixture::COURSE_3);
+        $this->client()->request(
+            'GET',
+            $this->generateUrl(self::ROUTE_APP_COURSE_PERMISSION_INDEX,
+                [
+                    'id' => $course->getId(),
+                ]
+            )
+        );
+
+        $token = $this->getCsrfToken('appbundle_add_course_info_permission');
+        $this->client()->request(
+            'POST',
+            $this->generateUrl(self::ROUTE_APP_COURSE_PERMISSION_INDEX,
+                [
+                    'id' => $course->getId(),
+                ]
+            ),
+            [
+                'appbundle_add_course_info_permission' => [
+                    "user" => $this->getUser()->getId(),
+                    "permission" => Permission::WRITE,
+                    "_token" => $token
+                ]
+            ]
+        );
+        /** @var CoursePermission $coursePermission */
+        $coursePermission = $this->getEntityManager()->getRepository(CoursePermission::class)->findOneBy(
+            [
+                'user' => $this->getUser(),
+                'permission' => Permission::WRITE
+            ]
+        );
+        $this->assertNotNull($coursePermission);
+    }
+
+    /**
+     *   The test failed because the permission adready exists
+     */
+    public function testAddPermissionSectionFailed()
+    {
+        $this->login();
+        $course = $this->getCourse();
+        $this->client()->request(
+            'GET',
+            $this->generateUrl(self::ROUTE_APP_COURSE_PERMISSION_INDEX,
+                [
+                    'id' => $course->getId(),
+                ]
+            )
+        );
+
+        $token = $this->getCsrfToken('appbundle_add_course_info_permission');
+        $this->client()->request(
+            'POST',
+            $this->generateUrl(self::ROUTE_APP_COURSE_PERMISSION_INDEX,
+                [
+                    'id' => $course->getId(),
+                ]
+            ),
+            [
+                'appbundle_add_course_info_permission' => [
+                    "user" => $this->getUser()->getId(),
+                    "permission" => Permission::WRITE,
+                    "_token" => $token
+                ]
+            ]
+        );
+        $coursePermissions = $this->getEntityManager()->getRepository(CoursePermission::class)->findBy(
+            [
+                'user' => $this->getUser(),
+                'permission' => Permission::WRITE,
+                'courseInfo' => $course
+            ]
+        );
+        self::assertEquals($this->count($coursePermissions), 1);
     }
 }
