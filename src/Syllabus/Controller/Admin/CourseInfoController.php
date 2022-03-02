0f8cd8688df129563487521e7e3ca7e69d49af97
Kevin Genes <kevin.genes@univ-cotedazur.fr>
2022-03-02 13:45:35 +0100

    test: admin/CourseInfoController - removed some deprecated functions

--- src/Syllabus/Controller/Admin/CourseInfoController.php
+++ src/Syllabus/Controller/Admin/CourseInfoController.php
@@ -4,18 +4,12 @@ namespace App\Syllabus\Controller\Admin;
 
 use App\Syllabus\Entity\Course;
 use App\Syllabus\Entity\CourseInfo;
-use App\Syllabus\Form\CourseInfo\CourseInfoAdminType;
-use App\Syllabus\Form\CourseInfo\DuplicateCourseInfoType;
-use App\Syllabus\Form\CourseInfo\ImportType;
+use App\Syllabus\Entity\Year;
 use App\Syllabus\Form\CourseInfoType;
-use App\Syllabus\Form\Filter\CourseInfoFilterType;
-use App\Syllabus\Helper\Report\Report;
-use App\Syllabus\Manager\CourseInfoManager;
 use App\Syllabus\Manager\StatisticSyllabusManager;
 use App\Syllabus\Repository\Doctrine\CourseInfoDoctrineRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
-use Lexik\Bundle\FormFilterBundle\Filter\FilterBuilderUpdaterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -31,128 +25,6 @@ use Symfony\Contracts\Translation\TranslatorInterface;
  */
 class CourseInfoController extends AbstractController
 {
-    /**
-     * @Route("/", name="index")
-     * @param Request $request
-     * @param CourseInfoDoctrineRepository $courseInfoDoctrineRepository
-     * @param FilterBuilderUpdaterInterface $filterBuilderUpdater
-     * @param CourseInfoManager $courseInfoManager
-     * @param EntityManagerInterface $em
-     * @param TranslatorInterface $translator
-     * @param PaginatorInterface $paginator
-     * @return RedirectResponse|Response
-     * @throws \Exception
-     */
-    public function indexAction(
-        Request $request,
-        CourseInfoDoctrineRepository $courseInfoDoctrineRepository,
-        FilterBuilderUpdaterInterface $filterBuilderUpdater,
-        CourseInfoManager $courseInfoManager,
-        EntityManagerInterface $em,
-        TranslatorInterface $translator,
-        PaginatorInterface $paginator
-    )
-    {
-        $qb = $courseInfoDoctrineRepository->getIndexQueryBuilder();
-
-        $form = $this->createForm(CourseInfoFilterType::class);
-        $duplicationForm = $this->createForm(DuplicateCourseInfoType::class);
-        $duplicationForm->handleRequest($request);
-
-        $isFormValid = true;
-        if ($duplicationForm->isSubmitted()) {
-
-            if ($duplicationForm->isValid()) {
-                $data = $duplicationForm->getData();
-                $from = $data['from'];
-
-                /** @var  CourseInfo $to */
-                $to = $data['to']->getCodeYear(true);
-
-
-                /** @var Report $report */
-                $report = $courseInfoManager->duplicate($from, $to, CourseInfoManager::DUPLICATION_CONTEXTE_MANUALLY);
-
-                if (!$report->hasMessages() and !$report->hasLines()) {
-
-                    $this->addFlash('success', $translator->trans('admin.course_info.flashbag.duplicate'));
-                    $em->flush();
-
-                    return $this->redirectToRoute('app.admin.course_info.index');
-                }
-
-                foreach ($report->getMessages() as $message) {
-                    $this->addFlash($message->getType(), $message->getContent());
-                }
-
-                foreach ($report->getLines() as $line) {
-                    foreach ($line->getComments() as $comment) {
-                        $this->addFlash('danger', $comment);
-                    }
-                }
-
-                return $this->redirectToRoute('app.admin_course.info.index');
-            }
-
-            $isFormValid = false;
-
-        }
-
-        if ($request->query->has($form->getName())) {
-
-            $form->submit($request->query->get($form->getName()));
-
-            $filterBuilderUpdater->setParts([
-                '__root__'    => 'ci',
-                'ci.course'      => 'c',
-                'ci.structure' => 's',
-                'ci.year' => 'y',
-            ]);
-
-            $filterBuilderUpdater->addFilterConditions($form, $qb);
-
-        }
-
-        $pagination = $paginator->paginate(
-            $qb,
-            $request->query->getInt('page', 1),
-            10
-        );
-
-        return $this->render('course_info/admin/index.html.twig', array(
-            'pagination' => $pagination,
-            'form' => $form->createView(),
-            'duplicationForm' => $duplicationForm->createView(),
-            'isFormValid' => $isFormValid
-        ));
-    }
-
-    /**
-     * @Route("/new", name="new")
-     *
-     * @param Request $request
-     * @param CourseInfoManager $courseInfoManager
-     * @param EntityManagerInterface $em
-     * @param TranslatorInterface $translator
-     * @return RedirectResponse|Response
-     * @throws \Exception
-     */
-    public function newAction(Request $request, CourseInfoManager $courseInfoManager, EntityManagerInterface $em, TranslatorInterface $translator)
-    {
-        $courseInfo = $courseInfoManager->new();
-
-        $form = $this->createForm(CourseInfoAdminType::class, $courseInfo, ['validation_groups' => ['Default', 'new']]);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() and $form->isValid()) {
-            $courseInfoManager->update($courseInfo);
-
-            $this->addFlash('success', $translator->trans('admin.course_info.flashbag.new'));
-            return $this->redirectToRoute('app.admin.course_info.index');
-        }
-        return $this->render('course_info/admin/new.html.twig', ['form' => $form->createView()]);
-    }
-
     /**
      * Update an existing CourseInfo
      *
@@ -189,17 +61,17 @@ class CourseInfoController extends AbstractController
      *
      * @Route("/published/{year}", name="published", methods={"GET", "POST"})
      *
-     * @param $year
+     * @param Year $year
      * @param Request $request
      * @param StatisticSyllabusManager $statisticSyllabusManager
      * @param PaginatorInterface $paginator
      * @return Response
      */
-    public function published($year, Request $request,StatisticSyllabusManager $statisticSyllabusManager,
+    public function published(Year $year, Request $request,StatisticSyllabusManager $statisticSyllabusManager,
                               PaginatorInterface $paginator)
     {
         $pagination = $paginator->paginate(
-            $statisticSyllabusManager->findSyllabusPublished($year),
+            $statisticSyllabusManager->findSyllabusPublished($year->getId()),
             $request->query->getInt('page', 1),
             10
         );
@@ -215,17 +87,17 @@ class CourseInfoController extends AbstractController
      *
      * @Route("/being-filled/{year}", name="being_filled", methods={"GET", "POST"})
      *
-     * @param $year
+     * @param Year $year
      * @param Request $request
      * @param StatisticSyllabusManager $statisticSyllabusManager
      * @param PaginatorInterface $paginator
      * @return Response
      */
-    public function beingFilled($year, Request $request,StatisticSyllabusManager $statisticSyllabusManager,
+    public function beingFilled(Year $year, Request $request,StatisticSyllabusManager $statisticSyllabusManager,
                               PaginatorInterface $paginator)
     {
         $pagination = $paginator->paginate(
-            $statisticSyllabusManager->findSyllabusBeingFilled($year),
+            $statisticSyllabusManager->findSyllabusBeingFilled($year->getId()),
             $request->query->getInt('page', 1),
             10
         );
@@ -233,35 +105,6 @@ class CourseInfoController extends AbstractController
         return $this->render('course_info/being_filled/being_filled.html.twig', array(
             'pagination' => $pagination
         ));
-
-    }
-
-    /**
-     * @Route("/duplicate-syllabus-from-file", name="duplicate_syllabus_from_file")
-     *
-     * @param CourseInfoManager $courseInfoManager
-     * @param Request $request
-     * @param EntityManagerInterface $em
-     * @return RedirectResponse|Response
-     * @throws \Exception
-     */
-    public function duplicateSyllabusFromFileAction(CourseInfoManager $courseInfoManager, Request $request, EntityManagerInterface $em)
-    {
-        $form = $this->createForm(ImportType::class);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() and $form->isValid())
-        {
-            $report = $courseInfoManager->duplicateFromFile($form->getData()['file']->getPathName());
-            $em->flush();
-
-            $request->getSession()->set('duplicateSyllabus', $report);
-            return $this->redirectToRoute('app.admin_course.info.duplicate_syllabus_from_file');
-        }
-        return $this->render('course_info/admin/duplicate_syllabus_from_file.html.twig', [
-            'form' => $form->createView(),
-            'report' => $request->getSession()->remove('duplicateSyllabus')
-        ]);
 
     }
 
