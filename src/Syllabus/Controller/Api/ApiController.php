691220ccd59910ef77a1cb7db0f13c76c5e4274d
Kevin Genes <kevin.genes@univ-cotedazur.fr>
2022-04-05 16:20:25 +0200

    feat: ApiController duplicate syllabus

--- src/Syllabus/Controller/Api/ApiController.php
+++ src/Syllabus/Controller/Api/ApiController.php
@@ -2,13 +2,21 @@
 
 namespace App\Syllabus\Controller\Api;
 
+use App\Syllabus\Entity\Course;
 use App\Syllabus\Entity\CourseInfo;
 use App\Syllabus\Entity\Year;
 use App\Syllabus\Exception\CourseInfoAlreadyExistException;
 use App\Syllabus\Exception\CourseInfoNotFoundException;
+use App\Syllabus\Exception\CourseNotFoundException;
 use App\Syllabus\Exception\YearNotFoundException;
+use App\Syllabus\Manager\CourseInfoManager;
+use App\Syllabus\Manager\YearManager;
+use App\Syllabus\Repository\Doctrine\CourseInfoDoctrineRepository;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
@@ -17,7 +25,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
  * Class CourseController
  * @package App\Syllabus\Controller
  *
- * @Route("course", name="app.api.")
+ * @Route("/api", name="app.api.")
  * @Security("is_granted('ROLE_API')")
  */
 class ApiController extends AbstractController
@@ -25,38 +33,60 @@ class ApiController extends AbstractController
     /**
      * @Route("/{code1}/{year1}/{code2}/{year2}", name="duplicate", methods={"GET"})
      * @Security("is_granted('ROLE_API_DUPLICATE_SYLLABUS')")
+     * @throws Exception
      */
-    public function duplicationSyllabusAction($code1, $year1, $code2, $year2, EntityManager $em)
+    public function duplicationSyllabusAction($code1, $year1, $code2, $year2,
+                                              EntityManagerInterface $em,
+                                              CourseInfoManager $courseInfoManager,
+                                              CourseInfoDoctrineRepository $courseInfoDoctrineRepository)
     {
-        $year = $em->getRepository(Year::class)->find($year1);
-        $nextYear = $em->getRepository(Year::class)->find($year2);
-
-        if(!$year instanceof Year || !$nextYear instanceof Year)
+        if(!$em->getRepository(Year::class)->find($year1) instanceof Year)
         {
-            throw new YearNotFoundException();
+            return $this->json(
+                'L\'année ' . $year1 . ' n\'a pas été trouvée',
+                Response::HTTP_NOT_FOUND
+            );
         }
 
-        $courseInfo = $em->getRepository(CourseInfo::class)->findOneBy([
-            'year' => $year,
-            'code' => $code1
-            // 'duplicateNextYear' => true
-        ]);
+        if(!$em->getRepository(Year::class)->find($year2) instanceof Year)
+        {
+            return $this->json(
+                'L\'année ' . $year2 . ' n\'a pas été trouvée',
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        $courseInfo = $courseInfoDoctrineRepository->findByCodeAndYear($code1, $year1);
+        // 'duplicateNextYear' => true
 
         if (!$courseInfo instanceof CourseInfo)
         {
-            throw new CourseInfoNotFoundException();
+            return $this->json(
+                'Le syllabus pour ' . $code1 . ' - ' . $year1 . ' n\'a pas été trouvé',
+                Response::HTTP_NOT_FOUND
+            );
         }
 
-        $nextCourseInfo = $em->getRepository(CourseInfo::class)->findOneBy([
-            'year' => $year2,
-            'code' => $code2
-        ]);
+        $nextCourseInfo = $courseInfoDoctrineRepository->findByCodeAndYear($code2, $year2);
 
-        if (!is_null($nextCourseInfo))
+        if (is_null($nextCourseInfo))
         {
-            throw new CourseInfoAlreadyExistException();
+            return $this->json(
+                'Le syllabus pour ' . $code2 . ' - ' . $year2 . ' n\'a pas été trouvé',
+                Response::HTTP_NOT_FOUND
+            );
         }
 
-        return;
+        $courseInfoManager->duplicate(
+            "{$code1}__UNION__{$year1}",
+            "{$code2}__UNION__{$year2}",
+            CourseInfoManager::DUPLICATION_CONTEXTE_MANUALLY
+        );
+
+        $em->flush();
+
+        //dd($courseInfoDoctrineRepository->findByCodeAndYear($code2, $year2));
+
+        return $this->json('');
     }
 }
